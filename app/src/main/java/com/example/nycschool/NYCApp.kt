f9dcac8f624f187fddada7c306576473eabc7747
Consultant <consultant@sysadmins-MacBook-Pro-4.local>
2022-08-10 14:55:09 -0400

    Solved error of login

--- app/src/main/java/com/example/nycschool/NYCApp.kt
+++ app/src/main/java/com/example/nycschool/NYCApp.kt
@@ -3,5 +3,5 @@ package com.example.nycschool
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-//@HiltAndroidApp
-//class NYCApp: Application() {}
+@HiltAndroidApp
+class NYCApp: Application()
